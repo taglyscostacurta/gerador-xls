403c37f86c66b9e855a933ee99990472f948346c
taglyscostacurta <taghcc@hotmail.com>
2022-12-15 17:19:01 -0300

    atualizacao colunas db

--- main.go
+++ main.go
@@ -14,10 +14,10 @@ func main() {
 	services.Generator() // gera o arquivo XLSX
 
 	//models.GenerateProducts()                      // gera os produtos
-	db.ConnectDB()                                 // conecta com o banco de dados
-	e := echo.New()                                // cria uma nova instância do Echo
-	e.GET("/products", controllers.GetProducts)    // cria uma rota para a função GetProducts
-	e.POST("/products", controllers.CreateProduct) // cria uma rota para a função CreateProduct
+	db.ConnectDB()                                // conecta com o banco de dados
+	e := echo.New()                               // cria uma nova instância do Echo
+	e.GET("/products", controllers.GetProducts)   // cria uma rota para a função GetProducts
+	e.POST("/product", controllers.CreateProduct) // cria uma rota para a função CreateProduct
 
 	e.Logger.Fatal(e.Start(":8000")) // inicia o servidor na porta 8000
 
--- models/models.go
+++ models/models.go
@@ -1,10 +1,19 @@
 package models
 
-type Product struct {
-	ID    uint    `json:"id" gorm:"primary_key"` // id do produto
-	Name  string  `json:"name"`                  // nome do produto
-	Price float64 `json:"price"`                 // preço do produto
+import (
+	"time"
 
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID        uint           `json:"id" gorm:"primarykey" AutoIncrement:"true"`
+	Name      string         `json:"name" validate:"nonzero"`
+	Price     uint           `json:"price" validate:"nonzero"`
+	Code      string         `json:"code" validate:"nonzero"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
 
 var Products []Product // slice de produtos
